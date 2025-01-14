b8c24a661b732fab6eeb4eb6f4bb580ec5b43fa9
avigupta250 <avinashkumarnav52@gmail.com>
2025-01-14 11:12:09 +0530

    fixes focus and highlighted diffenrece cell

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import SudokuSolver from './Suduko'
+
 import SudukoGrid from './Components/SudukoGrid'
 
 
--- src/Components/SudukoGrid.jsx
+++ src/Components/SudukoGrid.jsx
@@ -3,17 +3,15 @@ import React, { useRef, useState } from "react";
 const SudukoGrid = () => {
   const Grid = Array.from({ length: 9 }, () => Array(9).fill(""));
 
-  const [grid,setGrid]=useState(Grid);
+  const [grid, setGrid] = useState(Grid);
   const inputRefs = useRef([]);
+  const [beforeSolve, setBeforeSolve] = useState([]);
 
+  //   Grid?.map((row, rowInd) =>
+  //     row.map((col, colInd) => console.log(Grid[rowInd][colInd]))
+  //   );
 
-//   Grid?.map((row, rowInd) =>
-//     row.map((col, colInd) => console.log(Grid[rowInd][colInd]))
-//   );
-
-
-
-const isValid = (grid, row, col, num) => {
+  const isValid = (grid, row, col, num) => {
     for (let x = 0; x < 9; x++) {
       if (grid[row][x] == num || grid[x][col] == num) return false;
     }
@@ -27,66 +25,96 @@ const isValid = (grid, row, col, num) => {
     return true;
   };
 
-function solveSudoku(grid){
+  function solveSudoku(grid) {
+    for (let row = 0; row < 9; row++) {
+      for (let col = 0; col < 9; col++) {
+        if (grid[row][col] === "" || isNaN(grid[row][col])) {
+          for (let value = 1; value <= 9; value++) {
+            if (isValid(grid, row, col, value)) {
+              grid[row][col] = value;
 
-    for(let row=0;row<9;row++){
-        for(let col=0;col<9;col++){
-
-            if(grid[row][col]==="" ||isNaN( grid[row][col])){
-
-                for(let value=1;value<=9;value++){
-
-                    if(isValid(grid,row,col,value)){
-                        grid[row][col]=value;
-
-                       if(solveSudoku(grid))return true;
-                       grid[row][col]="";
-                    }
-                }
-                return false;
+              if (solveSudoku(grid)) return true;
+              grid[row][col] = "";
             }
-        }
-    }
-    return true;
-}
-const handleInputChange = (row, col,event, value) => {
-    const newGrid = grid.map((r) => [...r]);
-
-    if (event.key === "Backspace") {
-       
-        newGrid[row][col] = "";
-        setGrid(newGrid);
-  
-        
-      } else if (value === NaN || (/^[1-9]$/.test(value) && value.length === 1)) {
-        
-        newGrid[row][col] = value ? parseInt(value) : "";
-        setGrid(newGrid);
-  
-        const currentIndex = row * 9 + col;
-        const nextIndex = currentIndex + 1;
-        if (inputRefs.current[nextIndex]) {
-          inputRefs.current[nextIndex].focus();
+          }
+          return false;
         }
       }
+    }
+    return true;
+  }
+  const handleInputChange = (row, col, event, value) => {
+    const newGrid = grid.map((r) => [...r]);
+
+    if (event.key === " " || value === " ") {
+      const currentIndex = row * 9 + col;
+      const nextIndex = currentIndex + 1;
+
+      newGrid[row][col] = "";
+      setGrid(newGrid);
+
+      if (inputRefs.current[nextIndex]) {
+        inputRefs.current[nextIndex].focus();
+      }
+    } else if (/^[1-9]$/.test(value) && value.length === 1) {
+      newGrid[row][col] = parseInt(value);
+      setGrid(newGrid);
+     setBeforeSolve(newGrid);
+      const currentIndex = row * 9 + col;
+      const nextIndex = currentIndex + 1;
+
+      if (inputRefs.current[nextIndex]) {
+        inputRefs.current[nextIndex].focus();
+      }
+    }
+  };
+  const handleKeyDown = (row, col, event, value) => {
+    if (event.key == "Backspace" && grid[row][col] !== "") {
+      const newGrid = grid.map((r) => [...r]);
+      newGrid[row][col] = "";
+      setGrid(newGrid);
+      const currentIndex = row * 9 + col;
+      const prevIndex = currentIndex - 1;
+
+      if (inputRefs.current[prevIndex]) {
+        inputRefs.current[prevIndex].focus();
+      }
+    }
+    if (event.key == "Backspace" && grid[row][col] === "") {
+      const currentIndex = row * 9 + col;
+      const prevIndex = currentIndex - 1;
+
+      if (inputRefs.current[prevIndex]) {
+        inputRefs.current[prevIndex].focus();
+      }
+    }
   };
 
-const handleSolve = () => {
-    console.log("SolveSuduko called")
-    console.log(grid)
+  const handleSolve = () => {
+    console.log("SolveSuduko called");
+
+    // console.log(grid);
     const newGrid = grid.map((row) => row.slice());
+    setBeforeSolve(grid.map((row) => [...row]));
+
+    console.log("Before Solve:", beforeSolve);
+
+
     if (solveSudoku(newGrid)) {
       setGrid(newGrid);
+
+    //   setTimeout(()=>{
+    //     console.log("After Solve:", grid);
+    //   },1000)
+      console.log("After Solve:", grid);
     } else {
       alert("No solution exists for the given input.");
     }
   };
 
-  const handleReset=()=>{
-    
-        setGrid(Array.from({ length: 9 }, () => Array(9).fill("")));
-      
-  }
+  const handleReset = () => {
+    setGrid(Array.from({ length: 9 }, () => Array(9).fill("")));
+  };
 
   return (
     <div className="flex justify-center flex-col items-center w-full h-screen">
@@ -100,21 +128,38 @@ const handleSolve = () => {
               type="text"
               ref={(el) => (inputRefs.current[rowIndex * 9 + colIndex] = el)}
               value={value}
-              onChange={(e)=> handleInputChange(rowIndex,colIndex,e,e.target.value)}
-              onKeyDown={(e) =>
-                e.key === "Backspace" &&
-                handleInputChange(rowIndex, colIndex,e,"")
+              onKeyDown={(e) => handleKeyDown(rowIndex, colIndex, e, e.key)}
+              onChange={(e) =>
+                handleInputChange(rowIndex, colIndex, e, e.target.value)
               }
-             
-              className={ `${colIndex === 2 || colIndex === 5 ? "border-r-[3px] border-r-black" : ""} ${rowIndex === 2 || rowIndex === 5 ? "border-b-[3px] border-b-black" : ""} h-[40px] border text-black text-center border-red-500  w-[40px]`}
+              className={`
+    ${
+      beforeSolve.length > 0 &&
+      beforeSolve[rowIndex][colIndex] !== grid[rowIndex][colIndex] && grid[rowIndex][colIndex] !== ""
+        ? "text-blue-700"
+        : "text-black"
+    } 
+    ${colIndex === 2 || colIndex === 5 ? "border-r-[3px] border-r-black" : ""} 
+    ${rowIndex === 2 || rowIndex === 5 ? "border-b-[3px] border-b-black" : ""} 
+    h-[40px] border text-[30px] font-bold text-center border-red-500 w-[40px]`}
             />
           ))
         )}
       </div>
 
       <div className="flex mt-6 gap-5">
-        <button className="p-1 bg-gray-400 rounded-md px-5 " onClick={handleSolve} >Solve</button>
-        <button className="p-1 bg-gray-400 rounded-md px-5 " onClick={handleReset}>Reset</button>
+        <button
+          className="p-1 bg-gray-400 rounded-md px-5 "
+          onClick={handleSolve}
+        >
+          Solve
+        </button>
+        <button
+          className="p-1 bg-gray-400 rounded-md px-5 "
+          onClick={handleReset}
+        >
+          Reset
+        </button>
       </div>
     </div>
   );
